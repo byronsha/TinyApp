24456830bbc82cd1773b9b1c651f5f955e0f2b19
Byron Sha <byronysha@gmail.com>
2017-01-03 21:39:25 -0800

    First commit

---
+++ app/entry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div>Hey, it works! Nice!</div>,
+    document.getElementById('root')
+  )
+})
